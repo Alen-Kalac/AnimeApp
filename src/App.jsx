6235097e544712762657e0a9a0aa88b2718fb7fa
Alen-Kalac <alengames@live.com>
2023-08-02 20:44:24 +0200

    Finished sidebar,header,footer and styiling of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.scss'
+import { Route,Routes } from 'react-router-dom'
+import HomePage from './Pages/Home'
+import Header from './Components/Header'
+import AnimePage from './Pages/Anime'
+import Footer from './Components/Footer'
+import AboutUsPage from './Pages/AboutUs'
+import FavoritesPage from './Pages/Favorites'
+import MangaPage from './Pages/Manga'
+import SideBar from './Components/Sidebar'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header />
+    <div className="main">
+    <SideBar/>
+    <div className="main-content">
+      <Routes>
+    <Route path='/' element={<HomePage/>}/>
+    <Route path='/anime' element={<AnimePage/>}/>
+    <Route path='/manga' element={<MangaPage/>}/>
+    <Route path='/favorites' element={<FavoritesPage/>}/>
+    <Route path='/about-us' element={<AboutUsPage/>}/>
+    </Routes>
+    <Footer/>
+    </div>
+    </div>
     </>
+   
+   
   )
 }
 
