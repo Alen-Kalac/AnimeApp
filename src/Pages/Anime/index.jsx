b9311e0143e4474939f37b54d2cb0c3434eb495f
demirsubasic2001 <subasicd2000@gmail.com>
2023-08-08 20:29:07 +0200

    Added anime pages without logic for select option and search bar

--- src/Pages/Anime/index.jsx
+++ src/Pages/Anime/index.jsx
@@ -1,28 +1,30 @@
-import { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './index.scss'
+import './index.scss';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 function AnimePage() {
   const [animeList, setAnimeList] = useState([]);
   const [offset, setOffset] = useState(0);
-  const limit = 10; // Number of anime to fetch per request
+  const limit = 10;
+  const threshold = 200; // Threshold to trigger fetching more anime (in pixels)
 
   useEffect(() => {
-    // Function to fetch anime by genre and sort by popularity
     const fetchAnimeByGenre = async () => {
       try {
         const response = await axios.get(
           'https://kitsu.io/api/edge/anime',
           {
             params: {
-              'filter[genres]': "action", // Sortiranje po Zanru
-              'sort': 'popularityRank', // Sortiranje po popularnosti
-              'page[offset]': offset, // Broj anime/mangi po stranici
+              'filter[genres]': 'action',
+              'sort': 'popularityRank',
+              'page[offset]': offset,
             },
           }
         );
 
-       console.log(response.data.data) ;
+        setAnimeList((prevAnimeList) => [...prevAnimeList, ...response.data.data]);
       } catch (error) {
         console.error('Error fetching anime:', error);
       }
@@ -31,14 +33,62 @@ function AnimePage() {
     fetchAnimeByGenre();
   }, [offset]);
 
-  const handleLoadMore = () => {
-    setOffset((prevOffset) => prevOffset + limit);
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >=
+      document.body.scrollHeight - threshold
+    ) {
+      setOffset((prevOffset) => prevOffset + limit);
+    }
   };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
- <h1>Anime</h1>
+      <div className='anime-full-page'>
+        <div className='upper'>
+          <div className='list'>
+            <select name='Choose a category'>
+              <option value=''>Choose a category</option>
+              <option value='Adventure'>Adventure</option>
+              <option value='Action'>Action</option>
+              <option value='Fantasy'>Fantasy</option>
+              <option value='Crime'>Crime</option>
+              <option value='Drama'>Drama</option>
+              <option value='Romance'>Romance</option>
+              <option value='Supernatural'>Supernatural</option>
+              <option value='Magic'>Magic</option>
+              <option value='Horror'>Horror</option>
+            </select>
+          </div>
+          <div className='filter'>
+            <label>
+              <AiOutlineSearch />
+              <input type='text' className='search' placeholder='Search' />
+            </label>
+          </div>
+        </div>
+
+        <div className='cards'>
+        {animeList.map((anime) => (
+            <Link key={anime.id} to={`/anime/${anime.id}`} className='card-link'>
+              <div className='card'>
+                <img src={anime.attributes.posterImage.original} alt={anime.attributes.canonicalTitle} />
+                <div className="desc">
+                  <p className='title'>{anime.attributes.canonicalTitle}</p>
+                  <p className='click'>Click to see more</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default AnimePage;
